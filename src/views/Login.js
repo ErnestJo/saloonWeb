c030a7072e19db5f5fdde8964e0282ff232b749e
ErnestJo <smartlabstech@yahoo.com>
2022-12-20 19:55:15 +0300

    login form

---
+++ src/views/Login.js
@@ -0,0 +1,23 @@
+import Header from "../components/Header"
+import Login from "../components/Login"
+
+export default function LoginPage(){
+    return(
+        <>
+          <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full space-y-8">
+        
+           <Header
+                heading="Welcome to My Saloon"
+               // paragraph="Don't have an account yet? "
+                paragraph="Don't have an account yet? "
+                // linkName="Signup"
+                linkName="Contact Adminstation"
+                // linkUrl="/signup"
+                />
+            <Login/>
+        </div>
+        </div>      
+        </>
+    )
+}
